ef8676a765505a4dbe25a0fe79c044a68846f558
Federico A. Gorosito <federico.a.gorosito@gmail.com>
2025-05-23 12:45:53 -0300

    config secrets

--- PortfolioWeb/Servicios/ServicioEmail.cs
+++ PortfolioWeb/Servicios/ServicioEmail.cs
@@ -12,28 +12,40 @@ namespace PortfolioWeb.Servicios
 
     public class ServicioEmail(IConfiguration configuration) : IServicioEmail
     {
+
+
         public async Task EnviarEmail(ContactoViewModel contacto)
         {
-            var emailEmisor = configuration.GetValue<string>("CONFIGURACIONES_EMAIL:EMAIL");
-            var password = configuration.GetValue<string>("CONFIGURACIONES_EMAIL:PASSWORD");
-            var host = configuration.GetValue<string>("CONFIGURACIONES_EMAIL:HOST");
-            var puerto = configuration.GetValue<int>("CONFIGURACIONES_EMAIL:PUERTO");
+            try
+            {
+                var emailEmisor = configuration.GetValue<string>("CONFIGURACIONES_EMAIL:EMAIL");
+                var password = configuration.GetValue<string>("CONFIGURACIONES_EMAIL:PASSWORD");
+                var host = configuration.GetValue<string>("CONFIGURACIONES_EMAIL:HOST");
+                var puerto = configuration.GetValue<int>("CONFIGURACIONES_EMAIL:PUERTO");
 
-            var smtpCliente = new SmtpClient(host, puerto);
-            smtpCliente.EnableSsl = true;
-            smtpCliente.UseDefaultCredentials = false;
-
-
-            smtpCliente.Credentials = new NetworkCredential(emailEmisor, password);
-            var mensaje = new MailMessage(emailEmisor, emailEmisor, $"Nombre: {contacto.Nombre} Email:{contacto.Email}", contacto.Mensaje);
+                var smtpCliente = new SmtpClient(host, puerto);
+                smtpCliente.EnableSsl = true;
+                smtpCliente.UseDefaultCredentials = false;
 
 
-            await smtpCliente.SendMailAsync(mensaje);
+                smtpCliente.Credentials = new NetworkCredential(emailEmisor, password);
+                var mensaje = new MailMessage(emailEmisor, emailEmisor, $"Nombre: {contacto.Nombre} Email:{contacto.Email}", contacto.Mensaje);
 
+
+                await smtpCliente.SendMailAsync(mensaje);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
-    
-
-
 
     }
+
+
+
+
 }
+
